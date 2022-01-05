250fb5905dfac145113ea0b9fbd20cc1e239cd57
AyrtonCossuol <ayrton.cossuol@live.com>
2022-01-05 16:30:59 -0300

    inserted some initial properties and a step by step of the next tasks

--- generatePDF.js
+++ generatePDF.js
@@ -1,7 +1,22 @@
 function generatePDF(){
-    let pdf = new jspdf.jsPDF()
+    // Informando as propriedades de como será as configurações da folha do pdf.
+    const properties = {
+        orientation: 'p',
+        unit: 'mm',
+        format: 'a4',
+        putOnlyUsedFonts:true
+       }
 
-    pdf.autoTable({html: '#myTable'})
+    // Criando a instancia pdf
+    let pdf = new jspdf.jsPDF(properties)
     
+    // Criando um Cabeçalho padrão para o pdf.
+
+    // Gerando a tabela automaticamente com os dados vindo da tabela do HTML.
+    pdf.autoTable({html: '#myTable'})
+
+    // Adicionando algumas caracteristicas
+
+    // Salvando o relatorio em PDF.   
     pdf.save("Table.pdf")
 }
